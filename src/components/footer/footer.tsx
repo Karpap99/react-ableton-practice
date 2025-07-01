5e7d5967d9df8c6df23ca23a7fccb43ec76690f5
Sergiy_Karpichenko <151627147+Karpap99@users.noreply.github.com>
2025-07-01 23:31:45 +0300

    commit init

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,61 @@
+export const Footer = () => {
+    return(
+        <footer>
+            <h2>Ableton</h2>
+            <div>
+                <div>
+                    <ul>
+                        <li>
+                            Register Live or Push 
+                        </li>
+                        <li>
+                            About Ableton
+                        </li>
+                        <li>
+                            Jobs
+                        </li>
+                        <li>
+                            <div>
+                                <img alt='facebook'></img>
+                                <img alt='tweeter'></img>
+                                <img alt='youtube'></img>
+                                <img alt="instagram"></img>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>
+                        Education
+                    </h3>
+                    <ul>
+                       
+                        <li>Offers for students and teachers</li>
+                        <li>Offers for institutions</li>
+                        <li>Push 1 trade-in initiative</li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>Sign up to our newsletter</h3>
+                    <p>Enter your email address to stay up to date with latest offers, tutorials, downloads and more</p>
+                    <div>
+                        <input type="text"/>
+                        <div>
+                            Sign up
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <h3>
+                        Community
+                    </h3>
+                    <ul>
+                        <li>Find Ableton User Groups</li>
+                        <li>Find Certified Training</li>
+                        <li>Become a Certified Trainer</li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    )
+}
---
+++ src/components/header/header.tsx
@@ -0,0 +1,45 @@
+import './header.css'
+
+
+export const Header = () => {
+    return(
+    <header>
+        <div>
+            <ul>
+                <li>
+                    Live
+                </li>
+                <li>
+                    Push
+                </li>
+                <li>
+                    Link
+                </li>
+                <li>
+                    Shop
+                </li>
+                <li>
+                    Packs
+                </li>
+                <li>
+                    Help
+                </li>
+                <li>
+                    More +
+                </li>
+            </ul>
+        </div>
+        <ul>
+            <li>
+                Try Live for free
+            </li>
+            <li>
+                Account
+            </li>
+            <li>
+                Log out
+            </li>
+        </ul>
+    </header>
+    )
+}
